eef7e800249531d006ca870ae87e7af0db99c6f6
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-07 22:03:58 -0800

    Understaning more and more ways to make get through excercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,6 @@
 const repeatString = function(word, times) {
     if (times < 0) return "ERROR";
-    let string = "";
-    for (i = 0; i < times; i++) {
-        string += word;
-    } return string;
-
-
+    return word.repeat(times)
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(string) {
-    return string.split("").reverse().join("");
+return string.split("").reverse().join("");
+
 };
 
 // Do not edit below this line
 module.exports = reverseString;
-
